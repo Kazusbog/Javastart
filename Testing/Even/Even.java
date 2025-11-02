e45a195caf89f25a1ffcc48198ec215c733a0ced
Kazusbog <sweetcat3122@gmail.com>
2025-11-02 19:59:36 +0300

    added explain of code

--- Testing/Even/Even.java
+++ Testing/Even/Even.java
@@ -1,6 +1,6 @@
 public class Even{
     public static void main(String[] args) {
-        for (int i = 1; i<50; i++) {
+        for (int i = 1; i<50; i++) { // цикл начинается с числа 1, идет до 50 и увеличивается
             if (isEven(i)) {
                 System.out.println(i + " Четное");
             } else {
@@ -10,7 +10,7 @@ public class Even{
         }
     }
 
-    public static boolean isEven(int n) {
+    public static boolean isEven(int n) { // метод который считает четность числа
         return n%2 == 0;
     }   
 }
